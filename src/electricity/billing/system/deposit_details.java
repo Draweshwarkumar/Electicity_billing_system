e27d7f574da8e43fa2ed66863e0cb71ee913f38b
Draweshwarkumar <shivamjha22948@gmail.com>
2025-01-08 15:33:16 +0530

    .

---
+++ src/electricity/billing/system/deposit_details.java
@@ -0,0 +1,20 @@
+package electricity.billing.system;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class deposit_details extends JFrame implements ActionListener {
+    deposit_details(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+
+    public static void main(String[] args) {
+        new deposit_details();
+    }
+}
